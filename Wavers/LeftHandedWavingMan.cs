caef157dbdf57acb0c17a56a45b4ef0a3a45b41d
GET Academy <terje@kolderup.net>
2019-06-18 11:01:21 +0200

    x

---
+++ Wavers/LeftHandedWavingMan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wavers
+{
+    class LeftHandedWavingMan : WavingMan
+    {
+        public LeftHandedWavingMan(int x, int y) : base(x, y)
+        {
+        }
+
+        protected override void WriteHead()
+        {
+            Console.WriteLine(_armIsUp ? " o/" : " o");
+        }
+
+        protected override void WriteBody()
+        {
+            Console.WriteLine(_armIsUp ? "/|" : "/|\\");
+        }
+    }
+}
---
+++ Wavers/WavingMan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Wavers
+{
+    public class WavingMan
+    {
+        protected bool _armIsUp;
+        protected readonly int _x;
+        protected readonly int _y;
+
+        public WavingMan(int x, int y)
+        {
+            _y = y;
+            _x = x;
+        }
+
+        public void Wave()
+        {
+            _armIsUp = !_armIsUp;
+        }
+
+        public void Show()
+        {
+            Console.CursorTop = _y;
+            Console.CursorLeft = _x;
+            WriteHead();
+            Console.CursorLeft = _x;
+            WriteBody();
+            Console.CursorLeft = _x;
+            Console.Write("/ \\");
+        }
+
+        protected virtual void WriteBody()
+        {
+            Console.WriteLine(_armIsUp ? " |\\ " : "/|\\");
+        }
+
+        protected virtual void WriteHead()
+        {
+            Console.WriteLine(_armIsUp ? "\\o " : " o");
+        }
+    }
+}
